d65e8efb35906dc553958c142da8e64a9e9b693a
Michal Ciberej <michal.ciberej@gmail.com>
2024-04-23 20:41:48 +0200

    Basic setup

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,81 @@
 import express from 'express';
 import cors from 'cors';
-import bodyParser from 'body-parser';
+import passport from 'passport';
+import MongoStore from 'connect-mongo';
+import session from 'express-session';
+import mongoose from 'mongoose';
+import { User } from './models/user';
+import asyncHandler from 'express-async-handler';
 
+require('dotenv').config();
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors());
 app.set('views', 'src/views');
 app.set('view engine', 'pug');
 
+// app.use(
+//   session({
+//     secret: process.env.SECRET!,
+//     resave: false,
+//     saveUninitialized: true,
+//     store: MongoStore.create({ mongoUrl: process.env.DATABASE_URL }),
+//     cookie: {
+//       maxAge: 1000 * 60 * 60 * 24,
+//     },
+//   })
+// );
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
 app.get('/', (req, res) => {
-  res.send('Homepage');
+  res.render('index');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.post(
+  '/register',
+  asyncHandler(async (req, res) => {
+    // ! CHANGE FOR RETURN VALUE FROM HASH FUNCTION
+    const hashedPassword = '$';
+    const salt = '#';
+
+    const newUser = new User({
+      _id: new mongoose.Types.ObjectId(),
+      username: req.body.username,
+      hashedPassword,
+      salt,
+    });
+
+    try {
+      await newUser.save();
+      res.redirect('/login');
+    } catch (err) {
+      res.send('ERROR');
+    }
+  })
+);
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post(
+  '/login',
+  passport.authenticate('local', {
+    failureRedirect: '/register',
+    successRedirect: '/authPath',
+  })
+);
+
+app.get('/authPath', (req, res) => {
+  res.render('authPath');
 });
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
