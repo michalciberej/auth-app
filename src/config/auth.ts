b50e45632b0b438246164f3353ee5340eea404e4
Michal Ciberej <michal.ciberej@gmail.com>
2024-04-24 19:01:33 +0200

    Refactor serializeUser to pass user object

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,5 +1,5 @@
 import passport from 'passport';
-import { User } from '../models/user';
+import { User, UserType } from '../models/user';
 const LocalStrategy = require('passport-local').Strategy; // ?!
 import { validatePassword } from '../lib/password';
 
@@ -8,10 +8,9 @@ const verify = async (username: string, password: string, done: any) => {
     const user = await User.findOne({ username });
     if (!user) return done(null, false);
 
-    const isCorrectPassword = validatePassword(
+    const isCorrectPassword = await validatePassword(
       password,
-      user.hashedPassword,
-      user.salt
+      user.hashedPassword
     );
 
     if (isCorrectPassword) return done(null, user);
@@ -25,13 +24,14 @@ const strategy = new LocalStrategy(verify);
 
 passport.use(strategy);
 
-// passport.serializeUser((user, done) => {
-//   done(null, user.id);
-// });
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
 
-passport.deserializeUser(async (userId, done) => {
+passport.deserializeUser(async (userx: UserType, done) => {
+  const { username } = userx;
   try {
-    const user = await User.findById({ id: userId });
+    const user = await User.findOne({ username });
     done(null, user);
   } catch (err) {
     done(err);
